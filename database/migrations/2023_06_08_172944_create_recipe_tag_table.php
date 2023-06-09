00c4faf65b2efa87abfcc9866e2ecb5af83b5b93
emildobber <emildobber2@Hotmail.com>
2023-06-09 15:21:06 +0200

    added create recipe form and controller

--- database/migrations/2023_06_08_172944_create_recipe_tag_table.php
+++ database/migrations/2023_06_08_172944_create_recipe_tag_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('recipe_tag', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignIdFor(\App\Models\Recipe::class);
+            $table->foreignIdFor(\App\Models\Tag::class);
         });
     }
 
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('recipe_tag');
     }
 };
--- database/seeders/TestDataSeeder.php
+++ database/seeders/TestDataSeeder.php
@@ -13,11 +13,75 @@ class TestDataSeeder extends Seeder
      */
     public function run(): void
     {
+        //Users
         DB::table('users')->insert([
             'name' => 'Emil',
             'email' => 'emildobber2@hotmail.com',
             'password' => bcrypt('emildobber'),
             'current_team_id' => 1
+        ]);
+
+        //Tags
+        DB::table('tags')->insert([
+            'name' => 'Quick',
+        ]);
+
+        DB::table('tags')->insert([
+            'name' => 'Easy',
+        ]);
+
+        DB::table('tags')->insert([
+            'name' => 'One-pot',
+        ]);
+
+        //Origins
+        DB::table('origins')->insert([
+            'name' => 'Vietnam',
+        ]);
+
+        DB::table('origins')->insert([
+            'name' => 'Pakistan',
+        ]);
+
+        DB::table('origins')->insert([
+            'name' => 'Japan',
+        ]);
+
+
+        DB::table('ingredients')->insert([
+            'name' => 'Fish sauce',
+        ]);
+
+        DB::table('ingredients')->insert([
+            'name' => 'Ghee',
+        ]);
+
+        DB::table('ingredients')->insert([
+            'name' => 'Mirin',
+        ]);
+
+        DB::table('measurement_units')->insert([
+            'name' => 'ml',
+        ]);
+
+        DB::table('measurement_units')->insert([
+            'name' => 'l',
+        ]);
+
+        DB::table('measurement_units')->insert([
+            'name' => 'g',
+        ]);
+
+        DB::table('measurement_units')->insert([
+            'name' => 'kg',
+        ]);
+
+        DB::table('measurement_units')->insert([
+            'name' => 'Tablespoon',
+        ]);
+
+        DB::table('measurement_units')->insert([
+            'name' => 'Teaspoon',
         ]);
     }
 }
